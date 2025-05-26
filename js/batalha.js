9fef329a5ebf8d95895599e5b8507d9631919092
AmandhaYumi <mandhayumi@gmail>
2025-05-25 23:01:26 -0300

    "Adicionando barra de vida e progresso"

--- js/batalha.js
+++ js/batalha.js
@@ -1,3 +1,4 @@
+/*Variáveis e DOM*/
 let vidaGG = 100; 
 const vidaMaxima = 100;
 const totalBG = 100;
@@ -17,3 +18,36 @@ const mensagemFinal = document.getElementById("mensagemFinal");
 const spanAtaques = document.getElementById("ataquesRestantes");
 const barraVida = document.getElementById("barraVida");
 const barraBG = document.getElementById("barraBG");
+
+/*Progesso*/
+const salvarProgresso = () => {
+  const progresso = {
+    vidaGG,
+    BGRestantes,
+    BG,
+    limiteCura
+  };
+  localStorage.setItem("progressoGG", JSON.stringify(progresso));
+};
+
+const carregarProgresso = () => {
+  const progressoSalvo = localStorage.getItem("progressoGG");
+  if (progressoSalvo) {
+    const progresso = JSON.parse(progressoSalvo);
+    vidaGG = progresso.vidaGG;
+    BGRestantes = progresso.BGRestantes;
+    BG = progresso.BG;
+    limiteCura = progresso.limiteCura;
+  }
+};
+
+const atualizarTela = () => {
+  spanVida.textContent = vidaGG;
+  spanBG.textContent = BGRestantes;
+  if(spanAtaques) { 
+    spanAtaques.textContent = "";  
+  }
+  barraVida.style.width = `${(vidaGG / vidaMaxima) * 100}%`;
+  barraBG.style.width = `${(BGRestantes / totalBG) * 100}%`;
+  salvarProgresso();
+};
